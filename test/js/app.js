b3bce77cd8f9d0bd783ffe48087e3a9f3d42bb15
Yeison <yeisontorrado@termotecnica.com.co>
2017-05-10 17:32:26 -0500

    pruebas

--- test/js/app.js
+++ test/js/app.js
@@ -1,28 +1,16 @@
 $(document).foundation();
 
-function setWidths(selector){
-	var widths = [];
-	$( selector+" tbody tr:first-child td" ).each(function(indice){
-		widths[indice] = {w:$(this).outerWidth(), text: $(this).text() };
-	});
-	$( selector+" thead tr:first-child th ").each(function(indice){
-		var cell = widths[indice];
-		$(this).css( { "width": cell.w+"px" } );
-	});
-}
-
 function setHeader(selector1){
-	$(selector1+' tbody tr:first-child').clone().appendTo(selector1+' thead#thead2');
-	$(selector1+' tbody').css({"top": "-"+$(selector1+' thead#thead2').height()+"px"});
+	$(selector1+' tbody tr:last-child').clone().appendTo(selector1+' thead#thead2');
 	
 	$(selector1).css({ "width": $(selector1+" tbody").outerWidth()+"px" });
-	var widths = [];	
-	$(selector1+' tbody tr:last-child td').each(function(indice){
+	var widths = [];
+	$(selector1+' tbody tr:first-child td').each(function(indice){
 		widths[indice] = {w:$(this).outerWidth(), text: $(this).text() };
 		console.log(widths[indice]);
 	});
 	
-	$( selector1+' thead#thead2 tr:first-child th').each(function(indice){
+	$( selector1+' thead#thead2 tr:last-child th').each(function(indice){
 		var cell = widths[indice];
 		$(this).css( { "width": cell.w+"px" } );
 	});
