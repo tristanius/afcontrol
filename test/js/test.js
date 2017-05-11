d9a28f71f329e0c16235d179ca8035065d71f3d6
Yeison <yeisontorrado@termotecnica.com.co>
2017-05-11 18:05:41 -0500

    pruebas de panel

---
+++ test/js/test.js
@@ -0,0 +1,21 @@
+
+function setHeader(selector1){
+	$(selector1+' tbody tr:last-child').clone().appendTo(selector1+' thead#thead2');
+	
+	$(selector1).css({ "width": $(selector1+" tbody").outerWidth()+"px" });
+	var widths = [];
+	$(selector1+' tbody tr:first-child td').each(function(indice){
+		widths[indice] = {w:$(this).outerWidth(), text: $(this).text() };
+		console.log(widths[indice]);
+	});
+	
+	$( selector1+' thead#thead2 tr:last-child th').each(function(indice){
+		var cell = widths[indice];
+		$(this).css( { "width": cell.w+"px" } );
+	});
+}
+
+$(document).ready(function(){
+	setTimeout(setHeader("#LaTabla"), 2000);
+});
+
