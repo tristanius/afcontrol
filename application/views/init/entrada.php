3fe8e4759873da0f9521307518651e0c2a57201e
Yeison <yeisontorrado@termotecnica.com.co>
2017-05-22 17:53:16 -0500

    Entrada principal

--- application/views/init/entrada.php
+++ application/views/init/entrada.php
@@ -1,28 +1,41 @@
-<div class="">
+<div class="row" style="background: url('<?= base_url('assets/img/footer3.png') ?>'); background-size: 100% 100%; background-attachment: fixed;">
+
+	<section class="" style="margin:5px;">
+		<div class="callout">			
+			<p>AFControl, aplicación web para el majo de una lista de afiliados, con estados de afiliación, pagos y categorias de afiliación.</p>
+		</div>		
+	</section>
 	
-	<section class="callout primary" style="margin:1ex;">
-		<b>Afiliados:</b>
-		<div class="row">
-			<a href="" class="columns white-bg small-3 touch-btn"> <i class="fa fa-user-plus"></i> <p>Add. afiliado</p></a>
-			<a href="" class="columns white-bg small-3 touch-btn"> <i class="fa fa-eye"></i> <p>Ir a afiliado</p></a>
-			<a href="" class="columns white-bg small-3 touch-btn end"> <i class="fa fa-users"></i> <p>Listar afiliados</p></a>
+	<section class="columns medium-6">
+		<div class="callout" style="margin:5px;">
+			<b>Afiliados:</b>
+			<div class="row">
+				<a href="" class="columns white-bg small-3 touch-btn"> <i class="fa fa-user-plus"></i> <p>Add. afiliado</p></a>
+				<a href="" class="columns white-bg small-3 touch-btn"> <i class="fa fa-eye"></i> <p>Ir a afiliado</p></a>
+				<a href="" class="columns white-bg small-3 touch-btn end"> <i class="fa fa-users"></i> <p>Listar afiliados</p></a>
+			</div>
+		</div>			
+	</section>
+
+	<section class="columns medium-6">
+		<div class="callout" style="margin:5px;">
+			<b>Pagos:</b>
+			<div class="row">
+				<a href="" class="columns white-bg small-3 touch-btn"> <i class="fa fa-money"></i> <p>Pagos no viculantes</p></a>
+				<a href="" class="columns white-bg small-3 touch-btn"> <i class="fa fa-dollar"></i> <p>Pagos por afiliado</p></a>
+				<a href="" class="columns white-bg small-3 touch-btn end"> <i class="fa fa-list-alt "></i> <p>Listar pagos (todo)</p></a>
+			</div>
 		</div>
 	</section>
 
-	<section class="callout success" style="margin:1ex;">
-		<b>Pagos:</b>
-		<div class="row">
-			<a href="" class="columns white-bg small-3 touch-btn"> <i class="fa fa-money"></i> <p>Pagos no viculantes</p></a>
-			<a href="" class="columns white-bg small-3 touch-btn"> <i class="fa fa-dollar"></i> <p>Pagos por afiliado</p></a>
-			<a href="" class="columns white-bg small-3 touch-btn end"> <i class="fa fa-list-alt "></i> <p>Listar pagos (todo)</p></a>
-		</div>
-	</section>
-
-	<section class="callout light-gray" style="margin:1ex;">
-		<b>Consultas:</b>
-		<div class="row">
-			<a href="" class="columns white-bg small-3 touch-btn end"> <i class="fa fa-list-alt "></i> <p>Listar pagos (todo)</p></a>
-		</div>
+	<section class="columns medium-6 end">
+		<div class="callout" style="margin:5px;">
+			<b>Consultas:</b>
+			<div class="row">
+				<a href="" class="columns white-bg small-3 touch-btn end"> <i class="fa fa-table"></i> <p>consultas de afiliados</p></a>
+				<a href="" class="columns white-bg small-3 touch-btn end"> <i class="fa fa-file-excel-o"></i> <p>Exportar información</p></a>
+			</div>
+		</div>			
 	</section>
 
 </div>
